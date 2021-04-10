803bc774403a14a125d8563aa6dc415dd79a5889
kongxuhai <kongxuhai@126.com>
2021-04-10 21:41:25 +0800

    Initial commit

---
+++ 炸弹游戏.py
@@ -0,0 +1,39 @@
+# 确定玩家数量
+num_player = int(input("请确定玩家数："))
+# 确定范围下限
+num_min = int(input("请输入范围下限："))
+# 确定范围上限
+num_max = int(input("请输入范围上限："))
+# 确定玩家序号
+ser = 1
+# 判断数字范围是否正常
+while num_min > num_max:
+    print("范围限定异常")
+    num_max = int(input("请输入范围上限："))
+    num_min = int(input("请输入范围下限："))
+num_gogo = int(input("请输入1-100之间的目标数字："))
+while num_gogo >= num_max or num_gogo <= num_min:
+    print("目标数字超出范围")
+    num_gogo = int(input("请输入1-100之间的目标数字："))
+# 游戏开始
+print("游戏开始")
+# 游戏内容
+x = int(input("请" + str(ser) + "号玩家填入数字："))
+while x != num_gogo:
+    # 判断输入的数字是否在范围内
+    while x <= num_min or x >= num_max:
+        print("填入数字超出范围。")
+        x = int(input("请填入数字："))
+    # 判断输入数字应该改变范围上限还是下限
+    if x > num_gogo:
+        num_max = x
+    else:
+        num_min = x
+    if ser == num_player:
+        ser = 1
+    else:
+        ser = ser + 1
+    # 下一位玩家操作
+    print(num_min, "-", num_max)
+    x = int(input("请" + str(ser) + "号玩家填入数字："))
+print("你炸了！！！！！")
